de022b841addcb9a20678c55e461ce6e56e7426c
Камиль Рахматуллин <kamil.rahmatullin@gmail.com>
2025-10-31 01:01:31 +0300

    lab4

--- lab1/Primes.java
+++ lab1/Primes.java
@@ -1,6 +1,6 @@
 public class Primes {
     public static void main(String[] args) {
-        for (int i = 2; i < Integer.parseInt(args[0]); i++) {
+        for (int i = 2; i < 100; i++) {
             if (isPrime(i)) {
                 System.out.println(i);
             }
@@ -8,12 +8,11 @@ public class Primes {
     }
 
     public static boolean isPrime(int n) {
-        boolean isprime = true;
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
-                isprime = false;
+                return false;
             }
         }
-        return isprime;
+        return true;
     }
 }
